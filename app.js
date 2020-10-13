f53cf98ad6466b031e7f0b8a6fdbd47ee2e900a5
hitvardhan19 <hitvardhan.solanki@grabtaxi.com>
2020-10-14 02:43:18 +0530

    new app

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ const indexRouter = require('./routes/index');
 const newsRouter = require('./routes/news');
 
 const app = express();
-const corsOptions = { origin: false };
 
 app.use(logger('dev'));
-app.use(cors(corsOptions))
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
